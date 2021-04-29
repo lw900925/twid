6d8c1207597647347b319240b62d1ac451d836c2
lw900925 <lw900925@163.com>
2021-04-29 12:45:40 +0800

    优化日志打印

--- src/main/java/io/lw900925/download/app/AppRunner.java
+++ src/main/java/io/lw900925/download/app/AppRunner.java
@@ -237,14 +237,11 @@ public class AppRunner implements CommandLineRunner {
         for (String photoSize : photoSizes) {
             JsonElement sizeJsonElement = sizes.get(photoSize);
             if (sizeJsonElement != null) {
-                JsonObject size = sizeJsonElement.getAsJsonObject();
                 urlBuilder.append("?format=jpg&name=").append(photoSize);
-
-
                 // 图片大小：宽 x 高
-                String strSize = size.get("w").getAsString() + "x" + size.get("h").getAsString();
-
-                LOGGER.debug("图片URL解析成功，size:{}, URL:{}", strSize, urlBuilder);
+//                JsonObject size = sizeJsonElement.getAsJsonObject();
+//                String strSize = size.get("w").getAsString() + "x" + size.get("h").getAsString();
+//                LOGGER.debug("图片URL解析成功，size:{}, URL:{}", strSize, urlBuilder);
                 break;
             }
         }
